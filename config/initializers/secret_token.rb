6e7e4ec1ed9d9c2bce682dde869ebc0b7ced8565
nate.cui <cuiyingsan@gmail.com>
2010-10-11 11:35:35 +0800

    project created SP1-0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lbsc2::Application.config.secret_token = 'f3f0873432c020f56229e4bd742da3471073bfae39ab4881b71bcdf398aa9f7304c4f1d19377e9e8e61372fd2a8d21064610ffb11df391244502caa42c7dca57'
