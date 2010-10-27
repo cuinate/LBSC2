952307736309eb07b118897de8917b3c861517fe
nate.cui <cuiyingsan@gmail.com>
2010-10-27 21:56:18 +0800

    add sub_right_content for logged user

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,7 +5,9 @@ class Activity < ActiveRecord::Base
   scope :one_user, lambda { |user_id| where("user_id = ?", user_id) } 
   scope :one_place, lambda { |place_id| where("place_id = ?", place_id) } 
   scope :tagged_place, where(:action_id => 5)
+  scope :tagged_question, where(:action_id => 6)
   scope :place_id, select("place_id")
+  scope :question_id, select("question_id")
   scope :user_id, select("user_id")
 end
 
